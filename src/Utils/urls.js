cd848198d69a3d6d9aba921e1993a348d89c186a
Deepak <deepak99pmk@gmail.com>
2022-04-01 11:44:51 +0530

    added baseurl

--- src/Utils/urls.js
+++ src/Utils/urls.js
@@ -3,3 +3,4 @@ export const SIGNIN_URL = "http://localhost:5000/api/login";
 export const VERIFY_BASE_URL = "http://localhost:5000/api/verify";
 export const FORGET_PASSWORD_URL = "http://localhost:5000/api/forget-password";
 export const RESET_PASSWORD_URL = "http://localhost:5000/api/password-reset";
+export const BASE_URL = "http://localhost:5000";
