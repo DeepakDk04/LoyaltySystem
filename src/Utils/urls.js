79b831fdeead647c849562dcd9f366159624a78a
Deepak <deepak99pmk@gmail.com>
2022-04-20 22:14:56 +0530

    modified base urls for custom axios request

--- src/Utils/urls.js
+++ src/Utils/urls.js
@@ -1,6 +1,6 @@
-export const SIGNUP_URL = "http://localhost:5000/api/signup";
-export const SIGNIN_URL = "http://localhost:5000/api/login";
-export const VERIFY_BASE_URL = "http://localhost:5000/api/verify";
-export const FORGET_PASSWORD_URL = "http://localhost:5000/api/forget-password";
-export const RESET_PASSWORD_URL = "http://localhost:5000/api/password-reset";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "http://localhost:5000/";
+export const SIGNIN_URL = "auth/login";
+export const SIGNUP_URL = "auth/signup";
+export const VERIFY_BASE_URL = "auth/verify";
+export const FORGET_PASSWORD_URL = "auth/forget-password";
+export const RESET_PASSWORD_URL = "auth/password-reset";
